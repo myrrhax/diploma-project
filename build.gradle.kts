5465781a2dee53350e8b9034f95fe9b4cc0c581a
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-01-09 01:23:14 +0300

    Базовая настройка проекта и liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ group = "com.github.myrrhax"
 version = "0.0.1-SNAPSHOT"
 description = "Backend моей дипломной работы: Кооперативная система для разработки схем баз данных"
 
+val libVersions = mapOf(
+	"liquibase" to "5.0.1"
+)
+
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
@@ -26,11 +30,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	//implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	implementation("org.liquibase:liquibase-core:${libVersions["liquibase"]}")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
