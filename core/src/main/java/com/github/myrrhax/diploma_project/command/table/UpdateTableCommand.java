49b69fc2cf2b484ab6469becd3417492e73cc355
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-01-26 23:24:11 +0300

    Добавил обновление первичного ключа

--- core/src/main/java/com/github/myrrhax/diploma_project/command/table/UpdateTableCommand.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/command/table/UpdateTableCommand.java
@@ -7,7 +7,9 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Getter
@@ -17,17 +19,30 @@ public class UpdateTableCommand extends MetadataCommand {
     private UUID tableId;
     private String newTableName;
     private String newDescription;
+    private List<UUID> newPrimaryKeyParts;
 
     @Override
     public void execute(SchemaStateMetadata metadata) {
         TableMetadata table = metadata.getTable(tableId).orElse(null);
         Objects.requireNonNull(table);
 
-        if (newTableName != null && !newTableName.isBlank()) {
+        if (newTableName != null && !newTableName.isBlank() && !table.getName().equals(newTableName)) {
+            if (metadata.getTable(newTableName).isPresent()) {
+                throw new RuntimeException("Table with name " + newTableName + " already exists");
+            }
             table.setName(newTableName);
         }
         if (newDescription != null && !newDescription.isBlank()) {
             table.setDescription(newDescription);
+        }
+        if (newPrimaryKeyParts != null
+                && !newPrimaryKeyParts.isEmpty()
+                && newPrimaryKeyParts.stream().allMatch(kp -> table.getColumn(kp).isPresent())) {
+            table.setPrimaryKeyParts(newPrimaryKeyParts.stream()
+                    .map(table::getColumn)
+                    .map(Optional::get)
+                    .toList());
+            // ToDo Add reference invalidation
         }
     }
 }
