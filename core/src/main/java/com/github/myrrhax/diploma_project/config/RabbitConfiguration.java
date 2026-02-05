93e129f642b533848f69eb238540c0c3e5f0aac5
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-02-05 17:07:19 +0300

    Изменил настройку Rabbit-а, вынес в отдельный exchange. Также настроил CORS

--- core/src/main/java/com/github/myrrhax/diploma_project/config/RabbitConfiguration.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/config/RabbitConfiguration.java
@@ -1,5 +1,8 @@
 package com.github.myrrhax.diploma_project.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -11,10 +14,24 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfiguration {
     @Value("${app.rabbit.send-mail-queue}")
     private String mailQueue;
+    @Value("${app.rabbit.notification-exchange}")
+    private String exchangeName;
+    @Value("${app.rabbit.send-mail-key}")
+    private String sendMailRouterKey;
+
+    @Bean
+    public Exchange notificationExchange() {
+        return new DirectExchange(exchangeName, true, false);
+    }
 
     @Bean
     public Queue mailQueue() {
         return new Queue(mailQueue, true);
+    }
+
+    @Bean
+    public Binding mailNotificationBinding() {
+        return new Binding(mailQueue, Binding.DestinationType.QUEUE, exchangeName, sendMailRouterKey, null);
     }
 
     @Bean
