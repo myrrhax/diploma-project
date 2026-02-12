369cfdb134c8b2134e74c5e7e79c8b612cd8fe01
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-02-12 20:01:30 +0300

    Главная страница

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -1,36 +1,63 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './css/HomePage.css';
 import { schemaStore } from '../store/SchemaStore';
 import profilePic from '@/assets/user.png';
+import searchPic from '@/assets/search.png'
+import { schemaApi } from '../api/SchemaApiService';
+import { observer } from 'mobx-react-lite';
 
-export const HomePage = () => {
+export const HomePage = observer(() => {
+    const { schemas, isLoading } = schemaStore;
+    const [query, setQuery] = useState('');
+    const [authorOnly, setAuthorOnly] = useState(false);
+
+
+    
     useEffect(() => {
-        
+        schemaApi.loadUserSchemas();
     }, []);
+
+    const onSearchClick = async () => {
+        schemaApi.loadUserSchemas(authorOnly, query);
+    }
 
     return (
         <div className="home_page__container">
-            <h1 className="home_page__title">Ваши схемы:</h1>
-
-            {schemaStore.isLoading ? (
+            <div className='home_page__header'>
+                <h1 className="home_page__title">Ваши схемы:</h1>
+                <div className='home_page__schema_control'>
+                    <div className='schema_input'>
+                        <input type='text' 
+                            value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Имя схемы' />
+                        <img src={searchPic} onClick={onSearchClick} />
+                    </div>
+                    <div className='schema_search_mine'>
+                        <span>Только авторские</span>
+                        <input type='checkbox' checked={authorOnly} onChange={(e) => setAuthorOnly(e.target.checked)}  />
+                    </div>
+                </div>
+            </div>
+            
+            {isLoading ? (
                 <div className="loader">Загрузка...</div>
-            ) : schemaStore.schemas && schemaStore.schemas.length > 0 ? (
+            ) : schemas && schemas.length > 0 ? (
                 <div className="home_page__schema_list">
-                    {schemaStore.schemas.map((schema, _) => (
+                    {schemas.map((schema, _) => (
                         <div className='home_page__schema_element'>
                             <div className='schema_element__name'>{ schema.name }</div>
                             <div className='schema_element__creator'> 
-                                <img className='schema_element__creator__pic' src={profilePic} />
-                                <span className='schema_element__creator__name'>{ schema.creator.email }</span>
+                                <span className='author_label'>Автор: </span>
+                                <img src={profilePic} />
+                                <span className='author_name'>{ schema.creator.email }</span>
                             </div>
                         </div>
                     ))}
                 </div>
             ) : (
                 <div className="home_page__no_elements">
-                    Здесь пока ничего нет
+                    Здесь пока ничего нет, вы можете добавить схему или присоединиться по пригласительной ссылке
                 </div>
             )}
         </div>
     );
-};
+});
