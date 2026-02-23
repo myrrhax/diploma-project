4f842332ad955c2322ec6865ec97ed4a0d05c9b2
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-02-23 18:48:10 +0300

    Логи + правильные исключения для DeleteTableCommand

--- core/src/main/java/com/github/myrrhax/diploma_project/command/table/DeleteTableCommand.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/command/table/DeleteTableCommand.java
@@ -4,11 +4,15 @@ import com.github.myrrhax.diploma_project.command.MetadataCommand;
 import com.github.myrrhax.diploma_project.command.SchemaDifference;
 import com.github.myrrhax.diploma_project.model.SchemaStateMetadata;
 import com.github.myrrhax.diploma_project.model.TableMetadata;
+import com.github.myrrhax.diploma_project.model.enums.ErrorMessageKey;
+import com.github.myrrhax.diploma_project.model.exception.ApplicationException;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
 
+@Slf4j
 @Getter
 @Setter
 public class DeleteTableCommand extends MetadataCommand {
@@ -16,13 +20,19 @@ public class DeleteTableCommand extends MetadataCommand {
 
     @Override
     public SchemaDifference execute(SchemaStateMetadata metadata) {
-        TableMetadata table = metadata.getTable(tableId).orElseThrow();
+        log.info("Executing DeleteTableCommand for schema {}", schemeId);
+        TableMetadata table = metadata.getTable(tableId).orElseThrow(() -> {
+            log.info("Table {} is not found in schema {}", tableId, schemeId);
+            return new ApplicationException(ErrorMessageKey.TABLE_NOT_FOUND.getKey());
+        });
         metadata.removeTable(table);
+        log.info("Table {} was deleted from schema {}", tableId, schemeId);
         SchemaDifference diff = new SchemaDifference();
         diff.removeTable(tableId);
 
         SchemaDifference refDiff = metadata.deleteHangingReferences(table, true);
         diff.applyDifference(refDiff);
+        log.info("References was cascade deleted from after deleting table {}", tableId);
 
         return diff;
     }
