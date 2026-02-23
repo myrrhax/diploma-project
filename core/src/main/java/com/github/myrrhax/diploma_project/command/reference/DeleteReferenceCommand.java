2d80a856de84ffa7a0fc701e18c1a5a9d3490e58
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-02-23 19:46:00 +0300

    Убрал лишние импорты

--- core/src/main/java/com/github/myrrhax/diploma_project/command/reference/DeleteReferenceCommand.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/command/reference/DeleteReferenceCommand.java
@@ -11,8 +11,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Objects;
-
 @Slf4j
 @Getter
 @Setter
--- core/src/main/java/com/github/myrrhax/diploma_project/command/table/UpdateTableCommand.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/command/table/UpdateTableCommand.java
@@ -14,7 +14,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
