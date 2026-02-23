e291f608715c57ce62389b7d4dd2ec985ca3385f
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-02-23 19:07:55 +0300

    Exception handler для ошибок со стороны команд

--- core/src/main/java/com/github/myrrhax/diploma_project/web/GlobalExceptionHandler.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/web/GlobalExceptionHandler.java
@@ -4,17 +4,25 @@ import com.github.myrrhax.diploma_project.model.exception.ApplicationException;
 import com.github.myrrhax.diploma_project.model.dto.ErrorResponseDTO;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final MessageSource messageSource;
+
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponseDTO> handle(ApplicationException ex) {
         log.error("An application error occurred while processing the request: {}", ex.getMessage());
@@ -47,5 +55,14 @@ public class GlobalExceptionHandler {
         log.error("A fatal exception", ex);
 
         return ResponseEntity.internalServerError().build();
+    }
+
+    @MessageExceptionHandler(ApplicationException.class)
+    @SendToUser("/queue/errors")
+    public ErrorResponseDTO handleApplicationException(ApplicationException ex) {
+        log.error("An application error occurred while processing websocket command: {}", ex.getMessage());
+        String message = messageSource.getMessage(ex.getMessage(), ex.getArgs(), null);
+
+        return new ErrorResponseDTO(message, Collections.emptyMap());
     }
 }
