2eaff4b7a79ed467c93ee9dcf7ea4c6883e7967d
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-03-19 23:43:05 +0300

    Убрал лишние импорты с VersionController

--- core/src/main/java/com/github/myrrhax/diploma_project/web/VersionController.java
+++ core/src/main/java/com/github/myrrhax/diploma_project/web/VersionController.java
@@ -2,13 +2,11 @@ package com.github.myrrhax.diploma_project.web;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.github.myrrhax.diploma_project.model.dto.VersionDTO;
-import com.github.myrrhax.diploma_project.security.TokenUser;
 import com.github.myrrhax.diploma_project.service.VersionService;
 import com.github.myrrhax.diploma_project.util.ViewMarkers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
