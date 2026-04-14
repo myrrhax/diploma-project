fc4dfed3d92608e27d5c4bf555a89713ce1d64b9
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-04-14 22:04:44 +0300

    Script Name fix

--- ui/src/api/FileApiService.ts
+++ ui/src/api/FileApiService.ts
@@ -6,26 +6,13 @@ class FileApiService extends AbstractApiService {
         try {
             const response = await $api.get(`/files/${fileId}`, { responseType: 'blob' });
 
-            let filename = fallbackName;
-            const disposition = response.headers['content-disposition'];
-            
-            if (disposition) {
-                if (disposition.toLowerCase().includes("filename*=utf-8''")) {
-                    const match = disposition.match(/filename\*=utf-8''([^;]+)/i);
-                    if (match && match[1]) filename = decodeURIComponent(match[1].replace(/['"]/g, ''));
-                } else if (disposition.includes('filename=')) {
-                    const match = disposition.match(/filename=(["']?)([^"';]+)\1/i);
-                    if (match && match[2]) filename = decodeURIComponent(match[2]);
-                }
-            }
-
             const contentType = response.headers['content-type'] || 'application/octet-stream';
             const blob = new Blob([response.data], { type: contentType });
             const downloadUrl = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
             
             link.href = downloadUrl;
-            link.download = filename; 
+            link.download = fallbackName; 
             document.body.appendChild(link);
             link.click();
             
